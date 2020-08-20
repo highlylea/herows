8530d37dbad065e24a7dd3e681c4fffa50e7b443
zoze99 <jjginkgo@skku.edu>
2020-08-20 21:40:14 +0900

    style edit

--- pages/main_deck/main_deck.js
+++ pages/main_deck/main_deck.js
@@ -28,9 +28,39 @@ $(document).ready(function () {
         }
     });
 });
+
+
 // let d = document.querySelectorAll('.daily_date_id');
 
 // for (i=0; ++i; i<d.length) {
 //     let date = document.querySelectorAll('.daily_date_id')[i].id[3]+document.querySelectorAll('.daily_date_id')[i].id[4];
 //     document.querySelectorAll('.today_date')[i].innerText = date;
 //     }
+
+
+
+// Get the modal
+var modal = document.getElementById('block_modal');
+
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("modal_btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];                                          
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
