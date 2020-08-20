2611d09731537b275e5903bc8293d04ee6e07da6
hectic97 <hecticlife@g.skku.edu>
2020-08-20 21:44:34 +0900

    block past section

--- pages/main_deck/main_deck.js
+++ pages/main_deck/main_deck.js
@@ -1,35 +1,39 @@
-const today = new Date()
-const yesterday = new Date(today)
-var i = 0;
-document.querySelectorAll('.today_date').forEach(function (day) {
+function setBlockDay(){
+    const today = new Date()
+    const yesterday = new Date(today)
+    document.querySelectorAll('.today_date').forEach(function (day) {
     console.log(day.innerHTML);
     yesterday.setDate(yesterday.getDate() - 1);
     var date_str = yesterday.toDateString();
     day.innerHTML = date_str.split(' ')[2];
+    // day.innerHTML = '88';
+    
 });
-
+};
+setBlockDay();
 $(document).ready(function () {
     $('.log_section').scroll(function () {
         console.log('scroll!!!');
         if ($('.log_section').scrollTop() > 200) {
-            // $('.ufo').css('right','800px');
             $(".load_month").each(function () {
-                // $(this).innerText = 'over100!!';
                 this.innerHTML = '7월';
-                // Add $(this).val() to your list
             });
         }
         else {
             $(".load_month").each(function () {
-                // $(this).innerText = 'over100!!';
                 this.innerHTML = '8월';
-                // Add $(this).val() to your list
             });
-        }
+        };
     });
 });
 
-
+$('.load_past').click(function(){
+    var position = $('.log_section').scrollTop();
+    var a = $('.daily_date_id').clone()[0];
+    document.querySelector('.past_section').prepend(a);
+    $('.log_section')[0].scrollTop = position;
+    setBlockDay();
+});
 // let d = document.querySelectorAll('.daily_date_id');
 
 // for (i=0; ++i; i<d.length) {
