22a9c31505d3820fc388681b77098b2793c79787
hectic97 <hecticlife@g.skku.edu>
2020-08-20 17:43:12 +0900

    get Today date for block

--- pages/main_deck/main_deck.js
+++ pages/main_deck/main_deck.js
@@ -1,9 +1,16 @@
+const today = new Date()
+const yesterday = new Date(today)
+var i = 0;
+document.querySelectorAll('.today_date').forEach(function(day){
+    console.log(day.innerHTML);
+    yesterday.setDate(yesterday.getDate()-1);
+    var date_str = yesterday.toDateString();
+    day.innerHTML=date_str.split(' ')[2];
+});
 
+// let d = document.querySelectorAll('.daily_date_id');
 
-
-let d = document.querySelectorAll('.daily_date_id');
-
-for (i=0; ++i; i<d.length) {
-    let date = document.querySelectorAll('.daily_date_id')[i].id[3]+document.querySelectorAll('.daily_date_id')[i].id[4];
-    document.querySelectorAll('.today_date')[i].innerText = date;
-    }
+// for (i=0; ++i; i<d.length) {
+//     let date = document.querySelectorAll('.daily_date_id')[i].id[3]+document.querySelectorAll('.daily_date_id')[i].id[4];
+//     document.querySelectorAll('.today_date')[i].innerText = date;
+//     }
