686e5ee45a418b06aec80c0eef34259fceb734af
hectic97 <hecticlife@g.skku.edu>
2020-08-20 22:57:01 +0900

    fix block date starting from today

--- pages/main_deck/main_deck.js
+++ pages/main_deck/main_deck.js
@@ -1,16 +1,14 @@
-function setBlockDay(){
+function setBlockDay(isFirst) {
     const today = new Date()
     const yesterday = new Date(today)
+    yesterday.setDate(yesterday.getDate() + 1)
     document.querySelectorAll('.today_date').forEach(function (day) {
-    console.log(day.innerHTML);
-    yesterday.setDate(yesterday.getDate() - 1);
-    var date_str = yesterday.toDateString();
-    day.innerHTML = date_str.split(' ')[2];
-    // day.innerHTML = '88';
-    
-});
+        yesterday.setDate(yesterday.getDate() - 1);
+        var date_str = yesterday.toDateString();
+        day.innerHTML = date_str.split(' ')[2];
+    });
 };
-setBlockDay();
+setBlockDay(true);
 $(document).ready(function () {
     $('.log_section').scroll(function () {
         console.log('scroll!!!');
@@ -18,8 +16,7 @@ $(document).ready(function () {
             $(".load_month").each(function () {
                 this.innerHTML = '7월';
             });
-        }
-        else {
+        } else {
             $(".load_month").each(function () {
                 this.innerHTML = '8월';
             });
@@ -27,12 +24,12 @@ $(document).ready(function () {
     });
 });
 
-$('.load_past').click(function(){
+$('.load_past').click(function () {
     var position = $('.log_section').scrollTop();
     var a = $('.daily_date_id').clone()[0];
     document.querySelector('.past_section').prepend(a);
     $('.log_section')[0].scrollTop = position;
-    setBlockDay();
+    setBlockDay(false);
 });
 // let d = document.querySelectorAll('.daily_date_id');
 
@@ -50,20 +47,20 @@ var modal = document.getElementById('block_modal');
 var btn = document.getElementsByClassName("modal_btn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];                                          
+var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function() {
+btn.onclick = function () {
     modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
     modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
