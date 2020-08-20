764b8064f63c240ec9d9acd5d583163dcd1e04e7
zoze99 <jjginkgo@skku.edu>
2020-08-21 02:06:53 +0900

    backup

--- pages/main_deck/main_deck.js
+++ pages/main_deck/main_deck.js
@@ -38,27 +38,24 @@ $(document).ready(function () {
 //     }
 
 
+/*Block modal*/
 
-// Get the modal
-var modal = document.getElementById('block_modal');
+let modal = document.getElementById('block_modal');
+let btn = document.getElementsByClassName('modal_btn');
+let close_button = document.getElementsByClassName("close")[0];                                          
 
-// Get the button that opens the modal
-var btn = document.getElementsByClassName("modal_btn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];                                          
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
+for(i=0; ++i; i<btn.length){
+    btn[i].onclick = function() {
+        modal.style.display = "block";
+    }
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+
+close_button.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
