de6a8076140a71c08dbb1a0e1f4d38f119f2ee92
hectic97 <hecticlife@g.skku.edu>
2020-08-20 18:40:28 +0900

    block scroll month change

--- pages/main_deck/main_deck.js
+++ pages/main_deck/main_deck.js
@@ -1,13 +1,33 @@
 const today = new Date()
 const yesterday = new Date(today)
 var i = 0;
-document.querySelectorAll('.today_date').forEach(function(day){
+document.querySelectorAll('.today_date').forEach(function (day) {
     console.log(day.innerHTML);
-    yesterday.setDate(yesterday.getDate()-1);
+    yesterday.setDate(yesterday.getDate() - 1);
     var date_str = yesterday.toDateString();
-    day.innerHTML=date_str.split(' ')[2];
+    day.innerHTML = date_str.split(' ')[2];
 });
 
+$(document).ready(function () {
+    $('.log_section').scroll(function () {
+        console.log('scroll!!!');
+        if ($('.log_section').scrollTop() > 200) {
+            // $('.ufo').css('right','800px');
+            $(".load_month").each(function () {
+                // $(this).innerText = 'over100!!';
+                this.innerHTML = '7월';
+                // Add $(this).val() to your list
+            });
+        }
+        else {
+            $(".load_month").each(function () {
+                // $(this).innerText = 'over100!!';
+                this.innerHTML = '8월';
+                // Add $(this).val() to your list
+            });
+        }
+    });
+});
 // let d = document.querySelectorAll('.daily_date_id');
 
 // for (i=0; ++i; i<d.length) {
