2d2dff8fe8bfd51a36477702db3bea11969f170b
zoze99 <jjginkgo@skku.edu>
2020-08-20 17:22:27 +0900

    .micro_menu 완성 (.load_month, .load_future)

--- pages/main_deck/main_deck.js
+++ pages/main_deck/main_deck.js
@@ -1,4 +1,9 @@
 
 
 
-document.querySelectorAll('.daily_date_id')
+let d = document.querySelectorAll('.daily_date_id');
+
+for (i=0; ++i; i<d.length) {
+    let date = document.querySelectorAll('.daily_date_id')[i].id[3]+document.querySelectorAll('.daily_date_id')[i].id[4];
+    document.querySelectorAll('.today_date')[i].innerText = date;
+    }
