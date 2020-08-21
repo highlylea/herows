3a52dba9dc12109e13dc0acf2dace2eb3fc8a864
hectic97 <hecticlife@g.skku.edu>
2020-08-22 01:03:10 +0900

    datepicker d-day comment

--- pages/datepicker_practice/datepicker.js
+++ pages/datepicker_practice/datepicker.js
@@ -2,7 +2,29 @@ $( function() {
     $( "#datepicker" ).datepicker(
       );
   } );
-
+// console.log('fl')
 
     /* 카운트다운 */
+// class = ui-datepicker-month
+// class = ui-datepicker-year
+// class = ui-state-active
+$('#show_d-day').click(function(){
+  
+  var dday = $('.hasDatepicker')[0].value.split('/');
+  var deadline = new Date(`${dday[0]} ${dday[1]}, ${dday[2]} 00:00:00`).getTime();
+  var today = new Date();
+  today = today.getTime();
+  var distance = deadline - today;
+  var day_distance = Math.floor(distance / (1000 * 60 * 60 * 24));
+  if (distance == 0) {
+    document.querySelector('h1').innerHTML = "D-day";
+  }
+  else if (distance > 0){
+    document.querySelector('h1').innerHTML = 'D-'+(day_distance+1).toString();
+  } 
+  else{
+    document.querySelector('h1').innerHTML = 'D'+(day_distance+1).toString();
+  }
+
+})
 
