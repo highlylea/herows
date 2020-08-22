c378c42e30d30a8c4d2a8d6156cacb8e289d432b
dat.sci.seol <dat.sci.seol@gmail.com>
2020-08-22 20:48:07 +0900

    done

--- zoze/pages/main_deck/emoji_changer.js
+++
@@ -1,9 +0,0 @@
-$('.user5').find('.emoji').addClass('last_column_emoji');
-new FgEmojiPicker({
-    trigger:['.emoji'],
-    position: ['top', 'right'],
-    emit(obj, triggerElement) {
-        let emoji = obj.emoji;
-        triggerElement.innerText = emoji;
-    }
-});
