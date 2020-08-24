d812a4bb94fa9aa49a5cf0ede53c841a89847b65
deepdiver <time0909@g.skku.edu>
2020-08-25 03:53:11 +0900

    add link modal

--- pages/main_deck/main_deck.js
+++ pages/main_deck/main_deck.js
@@ -103,6 +103,22 @@ $(':radio').change(function() {
 });
   
 
+// 모달 첨부하기
+// $(document).ready(function(){ 
+//   var fileTarget = $('.filebox .upload-hidden'); 
+//   fileTarget.on('change', function(){ 
+//     // 값이 변경되면 
+//    if(window.FileReader){ 
+//       // 
+//       modern browser var filename = $(this)[0].files[0].name; 
+//   } else { // 
+//   old IE var filename = $(this).val().split('/').pop().split('\\').pop(); 
+//   // 파일명만 추출 
+// } 
+//   // 추출한 파일명 삽입 
+//     $(this).siblings('.upload-name').val(filename); 
+//   }); 
+// });
 
 
 
